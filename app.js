ae1eaf4424440eade4d6c3249c2de564e4fb9f81
Zemile30 <zemile.nesirli@gmail.com>
2025-05-18 19:04:33 +0000

    first

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 "use strict";
 $(document).ready(function () {
 
+$('.btn').click(function (e) { 
+    e.preventDefault();
+    
+    let a = (a) => {
+        for (let i = 1; i <= a; i++) {
+            $('.col-md-12').append(`<h3>${i}</h3>`);
+        }
+    }
+    a($('input').val())
+});
+
+
+
 });
