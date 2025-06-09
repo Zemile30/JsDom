05055883a0d575c61ce25941c78ddfda257b4eab
Zemile30 <zemile.nesirli@gmail.com>
2025-06-09 08:41:09 +0000

    end

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 "use strict";
 $(document).ready(function () {
 
-$('.btn').click(function (e) { 
-    e.preventDefault();
-    
-    let a = (a) => {
-        for (let i = 1; i <=a; i++) {
-            $('.col-md-12').append(`<h2>${i}</h2>`);
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        
+        let a = (a) => {
+            for (let i = 0; i < a; i++) {
+                $('.infobox').append(`<h2>${a}</h2>`);
+            }
         }
-    }
-    a($('input').val())
-});
+        a($('input').val())
+    });
+
+    $('.btn').click(function (e) { 
+        e.preventDefault();
+        
+        let b = (b) => {
+        $('.infobox').append(`<h2>${b}</h2>`);
+        }
+    
+        // b($('input').val())
+        
+    });
 
 
 });
