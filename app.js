1ff620483b916a49323e2f63ad645da82d63ad88
Zemile30 <zemile.nesirli@gmail.com>
2025-06-18 12:28:18 +0000

    end end

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ $(document).ready(function () {
 
     $('.btn').click(function (e) { 
         e.preventDefault();
-        if ($.trim($('input').val()) == "") {
-            $('input').val();
-        } else {
+        if (!$.trim($('input').val()) == "") {
             let k = (a) => {
                 for (let i = 0; i < a; i++) {$('.infobox').append(`<h2>${a}</h2>`)};            
             }
             let b = (e) => Number(e) ? k(e) : $('.infobox').append(`<h2>${e}</h2>`);
             b($('input').val());
+        }
+        else {
+            return
         }
     
         $('input').val('')
